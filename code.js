58248d9fc42f59cdb27c5d8d19734ea499da9b54
gary <gary.white@lodgit.com>
2021-11-10 16:30:47 +0100

    code working

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
 function sumArray(array) {
     var sum = 0;
-    var lowNum = Math.min(...array);
-    var highNum = Math.max(...array);
-    const reducer = (previousValue, currentValue) => previousValue + currentValue;
-    var newArray = array.reduce(reducer);
-    // if (array.length <= 1 ){
-    //     return 0;
-    // } else {
-    //     for (var i = 0; i < array.length; i++){
-    //         sum = sum + array[i];
-    //     }
-    // }
-    return newArray - lowNum - highNum;
+    if (array == null || array.length <= 1){ // check if array is full
+        return 0;
+    } else {
+        for (var i = 0; i < array.length; i++){
+            sum = sum + array[i]; // total the array
+        }
+    }
+    var lowNum = Math.min(...array); // find lowest num
+    var highNum = Math.max(...array); // find highest num
+    return sum - lowNum - highNum; // subtract high/lowest from total
 }
 
 console.log(sumArray([ 6, 2, 1, 8, 10 ])); // Returns 16
 console.log(sumArray([ 1, 1, 11, 2, 3 ])); // Returns 6
 console.log(sumArray([ 1])); // Returns 0
 console.log(sumArray([])); // Returns 0
+console.log(sumArray([2, 2])); // Returns 0
+console.log(sumArray(null)); // Returns 0
