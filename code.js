f5280f9f980daef5ae01b1ba9ef4f23da898bb8f
gary <gary.white@lodgit.com>
2021-11-10 14:26:18 +0100

    changes to code

--- code.js
+++ code.js
@@ -2,14 +2,16 @@ function sumArray(array) {
     var sum = 0;
     var lowNum = Math.min(...array);
     var highNum = Math.max(...array);
-    if (array.length <= 1 ){
-        return 0;
-    } else {
-        for (var i = 0; i < array.length; i++){
-            sum = sum + array[i];
-        }
-    }
-    return sum - lowNum - highNum;
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    var newArray = array.reduce(reducer);
+    // if (array.length <= 1 ){
+    //     return 0;
+    // } else {
+    //     for (var i = 0; i < array.length; i++){
+    //         sum = sum + array[i];
+    //     }
+    // }
+    return newArray - lowNum - highNum;
 }
 
 console.log(sumArray([ 6, 2, 1, 8, 10 ])); // Returns 16
