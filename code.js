f58121e1bcaedf1b072556fa120bff2d3913db07
gary <gary.white@lodgit.com>
2021-11-10 13:47:10 +0100

    code working

--- code.js
+++ code.js
@@ -1,6 +1,18 @@
 function sumArray(array) {
+    var sum = 0;
     var lowNum = Math.min(...array);
-    return lowNum;
+    var highNum = Math.max(...array);
+    if (array.length <= 1 ){
+        return 0;
+    } else {
+        for (var i = 0; i < array.length; i++){
+            sum = sum + array[i];
+        }
+    }
+    return sum - lowNum - highNum;
 }
 
 console.log(sumArray([ 6, 2, 1, 8, 10 ])); // Returns 16
+console.log(sumArray([ 1, 1, 11, 2, 3 ])); // Returns 6
+console.log(sumArray([ 1])); // Returns 0
+console.log(sumArray([])); // Returns 0
